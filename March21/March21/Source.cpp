b62ebc8df6cee7a894b80f0d73b7b39cb35c3fb1
Game110AM <Game110AM@605-110-STU11>
2017-03-21 07:56:46 -0600

    eye

---
+++ March21/March21/Source.cpp
@@ -0,0 +1,20 @@
+#include <iostream>   //Made by Jose Medina and Teodoro Martinez
+using namespace std;
+#include <string>
+int main() {
+	string game[5];
+	cout << "What is your favortie game good sir?" << endl;
+	for (int i = 0; i < 5; i++) {
+		getline(cin, game[i]);
+		if (game[i].compare("E.T.") == 0)
+			cout << "Hey!  I love that game!  You're pretty cool!" << endl;
+		else if (game[i].compare("Overwatch") == 0)
+			cout << "You're weird I dont like you" << endl;
+	}
+	cout << "So, your favorite games are..." << endl;
+	for (int i = 0; i < 5; i++) {
+		cout << game[i] << endl;
+
+	}
+	system("Pause");
+}
